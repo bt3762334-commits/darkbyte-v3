c2310ac2e66b874aa44236605f498f091040607c
bt3762334-commits <bt3762334@gmail.com>
2026-06-01 18:53:33 +0300

    Create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,16 @@
+import CopyPhone from "./CopyPhone";
+
+export default function Contact() {
+
+  return (
+    <section className="py-20 text-center">
+
+      <h2 className="basem taha">
+        Contact Me
+      </h2>
+
+      <CopyPhone />
+
+    </section>
+  );
+}
